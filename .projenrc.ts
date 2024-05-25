e0b60353887f203f06ee05cef6be025417fa8b46
Cameron Childress <cameronc@sumoc.com>
2024-05-25 10:06:53 -0400

    added vscode

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,13 @@
-import { typescript } from 'projen';
+import { typescript } from "projen";
+import {
+  VsCode,
+  VsCodeRecommendedExtensions,
+  VsCodeSettings,
+} from "projen/lib/vscode";
+
 const project = new typescript.TypeScriptAppProject({
-  defaultReleaseBranch: 'main',
-  name: 'cameronchildress.com',
+  defaultReleaseBranch: "main",
+  name: "cameronchildress.com",
   projenrcTs: true,
 
   prettier: true,
@@ -11,4 +17,28 @@ const project = new typescript.TypeScriptAppProject({
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+const vscode = new VsCode(project);
+
+// VSCODE: Root level editor settings
+const vsSettings = new VsCodeSettings(vscode);
+vsSettings.addSetting("editor.tabSize", 2);
+vsSettings.addSetting("editor.bracketPairColorization.enabled", true);
+vsSettings.addSetting("editor.guides.bracketPairs", "active");
+vsSettings.addSetting("editor.rulers", [80, 120]);
+
+// use eslint to fix files for typescript files only
+vsSettings.addSetting(
+  "editor.codeActionsOnSave",
+  { "source.fixAll.eslint": "explicit" },
+  "typescript",
+);
+
+// In case of monorepo, make sure each directory's eslint is found properly.
+vsSettings.addSetting("eslint.workingDirectories", [{ mode: "auto" }]);
+
+// VSCODE: extensions
+const vsExtensions = new VsCodeRecommendedExtensions(vscode);
+vsExtensions.addRecommendations("dbaeumer.vscode-eslint");
+
 project.synth();
