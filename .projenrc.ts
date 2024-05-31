7f8e67f0182e8cd5ff61c0dc964d73cf0ddd457f
Cameron Childress <cameronc@sumoc.com>
2024-05-31 11:24:53 -0400

    misc commends and updates

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,97 +1,219 @@
 import { typescript } from "projen";
 import { Job, JobPermission } from "projen/lib/github/workflows-model";
 import { NodePackageManager } from "projen/lib/javascript";
-import {
-  VsCode,
-  VsCodeRecommendedExtensions,
-  VsCodeSettings,
-} from "projen/lib/vscode";
+import { VsCode, VsCodeSettings } from "projen/lib/vscode";
 
 /*******************************************************************************
- * Netlify Config
+ *
+ * VitePress Options
+ *
+ * Some options we'll use to configure vitepress in this project.
+ *
  ******************************************************************************/
 
-const NETLIFY_SITE_ID = "7840347a-1605-469f-878f-bc76c7333db4";
-const NETLIFY_AUTH_TOKEN = "${{ secrets.NETLIFY_AUTH_TOKEN }}";
-const NETLIFY_DEPLOY_DIR = "content/.vitepress/dist";
+/**
+ * The subdirectory where the VitePress site itself is located. This value is
+ * used to generate lots of other configuration options, including CI workflows.
+ */
+const VITEPRESS_SITE_DIR = "content";
+
+/*******************************************************************************
+ *
+ * Netlify Options
+ *
+ * These are the values required for github actions generated at the end of this
+ * file.
+ *
+ ******************************************************************************/
 
 /**
- * Projen Config
+ * The Netlify Site ID is a unique identifier for your site. You can find this
+ * value in the Netlify dashboard for your site and replace it with your own
+ * Site ID.
+ *
+ * This is not considered a secret.
  */
+const NETLIFY_SITE_ID = "7840347a-1605-469f-878f-bc76c7333db4";
+
+/**
+ * Notably, we aren't using Netlify to build the site. Instead we're using
+ * GitHub Actions for our CI pipeline. The Netlify Auth Token is a secret that
+ * you will need to create in your GitHub account and named as shown here.
+ */
+const NETLIFY_AUTH_TOKEN = "${{ secrets.NETLIFY_AUTH_TOKEN }}";
+
+/**
+ * The directory where the VitePress build artifacts are located. This is used
+ * to tell various parts of Projen where to expect build outputs to be located.
+ */
+const NETLIFY_DEPLOY_DIR = `${VITEPRESS_SITE_DIR}/.vitepress/dist`;
+
+/*******************************************************************************
+ *
+ * Projen Config
+ *
+ * We're using the default TypeScript project, but we're adding some additional
+ * configuration to support VitePress. This configures the other app project
+ * options. We will also be adding more VitePress specific configurations later
+ * in this file.
+ *
+ ******************************************************************************/
+
 const project = new typescript.TypeScriptAppProject({
+  /**
+   * The default branch that production deploys will come from. Historically
+   * this was master but a more modern option is main. Projen requires this to
+   * be manually set for historical backward compatibility reasons.
+   */
   defaultReleaseBranch: "main",
+
+  /**
+   * This will become the name of the project in the package.json file.
+   */
   name: "cameronchildress.com",
+
+  /**
+   * Enable the projenrc.ts file. Historically, Projen used a projenrc.js file,
+   * so we set this manually to tell Projen we're really serious about using
+   * Typescript.
+   */
   projenrcTs: true,
 
+  /**
+   * Enable Prettier for code formatting. This keeps our code tight and
+   * consistent.
+   */
   prettier: true,
 
+  /**
+   * Use PNPM instead of the default of Yarn. This is a personal preference.
+   */
   packageManager: NodePackageManager.PNPM,
   pnpmVersion: "9",
 
-  devDeps: ["vitepress"],
-
+  /**
+   *
+   * Projen generates a lot of configuration files for you. This is great but
+   * to have deterministic builds, your don't want any of those generated files
+   * to change during your CI process.
+   *
+   * Setting mutable builds to false means that if the build script causes any
+   * changes, including to configuration files or snapshot tests, the CI build
+   * will fail.
+   *
+   * This is a great safety net to prevent unexpected outcomes in your CI/CD
+   * pipeline.
+   */
   buildWorkflowOptions: {
     mutableBuild: false,
   },
 
+  /**
+   * This is the directory where Projen expects to find build artifacts. This
+   * directory value is used in generated GitHub Actions and other CI
+   * configurations as the artifact directory that gets bundled up and saved to
+   * be used in future CI steps or stages.
+   *
+   * In the case of this particular configuration of VitePress, the build
+   * artifacts are in the `content/.vitepress/dist`
+   */
   artifactsDirectory: NETLIFY_DEPLOY_DIR,
 
   /**
-   * Build on pr, manually, and also when main is pushed (aka: PR approved and merged)
+   * Defined the GitHub Action triggers for the project. In this case we want
+   * CI workflows to be triggered when:
+   *
+   * - A PR is opened
+   * - A manual workflow is triggered in GitHub's UI
+   * - When code is pushed to the main branch (a production deploy)
    */
   buildWorkflowTriggers: {
     pullRequest: {},
     workflowDispatch: {},
     push: { branches: ["main"] },
   },
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 
-/**
- * VitePress Configuration
- */
+/*******************************************************************************
+ *
+ * VitePress Config
+ *
+ * Some of these options could be specified in the above project configuration,
+ * but I prefer to keep them separate for clarity so that I know which configs
+ * are specific to VitePress and which might be useful in other types of
+ * projects.
+ *
+ ******************************************************************************/
 
-// Exclude Vitepress build artifacts from git
+/**
+ * Add vitepress as a dev dependency for the project. We aren't going to
+ * specify a version here because we'll be using the auto-update workflow
+ * features of Projen to keep packages all up to date.
+ */
+project.addDevDeps("vitepress");
+
+/**
+ * When you install vitepress using `vitepress init`, you're told to exclude
+ * these two folders in your gitignore. Since we're using Projen we add them
+ * here and Projen takes case of the rest.
+ */
 project.gitignore.exclude(
-  "content/.vitepress/dist",
-  "content/.vitepress/cache",
+  `${VITEPRESS_SITE_DIR}/.vitepress/dist`,
+  `${VITEPRESS_SITE_DIR}/.vitepress/cache`,
 );
 
-// Add Vitepress tasks
-project.addTask("content:dev").exec("vitepress dev content");
-project.addTask("content:build").exec("vitepress build content");
-project.addTask("content:preview").exec("vitepress preview content");
+/**
+ * Add the typical 3 VitePress tasks, prefixed with the folder name where our
+ * site content is located.
+ */
+["dev", "build", "preview"].forEach((task) => {
+  project
+    .addTask(`${VITEPRESS_SITE_DIR}:${task}`)
+    .exec(`vitepress ${task} ${VITEPRESS_SITE_DIR}`);
+});
+
+/*******************************************************************************
+ *
+ * VS Code Config
+ *
+ * We've configured Prettier and ESLint for this project, but to get the full
+ * benefit we also need to tell VS Code to lint our code as we do our work. This
+ * section builds aVS Code configuration files that will be generated by Projen
+ * into the project's .vscode directory.
+ *
+ ******************************************************************************/
 
 /**
- * VSCode Configuration
+ * Projen includes a VSCode component that does most of the heavy listing. We
+ * access it here.
  */
-
 const vscode = new VsCode(project);
 
-// VSCODE: Root level editor settings
+/**
+ * Add som basic configuration settings to the VS Code settings file:
+ *
+ * - Set the tab size to 2 spaces
+ * - Enable bracket pair colorization
+ * - Highlight active bracket pairs
+ * - Add rulers at 80 and 120 characters
+ */
 const vsSettings = new VsCodeSettings(vscode);
 vsSettings.addSetting("editor.tabSize", 2);
 vsSettings.addSetting("editor.bracketPairColorization.enabled", true);
 vsSettings.addSetting("editor.guides.bracketPairs", "active");
 vsSettings.addSetting("editor.rulers", [80, 120]);
 
-// use eslint to fix files for typescript files only
+/**
+ * Add some ESLint specific settings to the VS Code settings file. Here we
+ * set the ESLint code actions to run on save and also make sure we're only
+ * linting typescript files to speed up eslint. Without this specificity, eslint
+ * would lint all files in the project and feel super sluggish.
+ */
 vsSettings.addSetting(
   "editor.codeActionsOnSave",
   { "source.fixAll.eslint": "explicit" },
   "typescript",
 );
-
-// In case of monorepo, make sure each directory's eslint is found properly.
-vsSettings.addSetting("eslint.workingDirectories", [{ mode: "auto" }]);
-
-// VSCODE: extensions
-const vsExtensions = new VsCodeRecommendedExtensions(vscode);
-vsExtensions.addRecommendations("dbaeumer.vscode-eslint");
 
 /*******************************************************************************
  *
@@ -103,7 +225,18 @@ vsExtensions.addRecommendations("dbaeumer.vscode-eslint");
  *
  ******************************************************************************/
 
-project.compileTask.exec("npx projen content:build");
+/**
+ * By default, projen runs `tsc --build` as part of it's compile step. We don't
+ * actually want to do that since vitepress will use vite for this instead. So
+ * we reset the compile task to remove the default tsc build command.
+ */
+project.compileTask.reset();
+
+/**
+ * Add a new compile task that runs the VitePress build command. This replaces
+ * the default tsc command with what we'd expect to see on a VitePress site.
+ */
+project.compileTask.exec(`npx projen ${VITEPRESS_SITE_DIR}:build`);
 
 /*******************************************************************************
  *
@@ -117,10 +250,6 @@ project.compileTask.exec("npx projen content:build");
 interface DeployJobOptions {
   isPr?: boolean;
   jobName?: string;
-  /**
-   * @default "main"
-   */
-  productionBranch?: string;
 }
 
 const deployJob = (options: DeployJobOptions): Job => {
@@ -130,7 +259,6 @@ const deployJob = (options: DeployJobOptions): Job => {
   const isPr = options.isPr ?? true;
   const jobName =
     options.jobName ?? options.isPr ? "deploy-preview" : "deploy-main";
-  const productionBranch = options.productionBranch ?? "main";
   const netlifyPreviewUrl = isPr
     ? "${{ steps.netlify-deploy.outputs.NETLIFY_URL }}"
     : "${{ steps.netlify-deploy.outputs.NETLIFY_LIVE_URL }}";
@@ -145,7 +273,7 @@ const deployJob = (options: DeployJobOptions): Job => {
     },
     if: isPr
       ? `startsWith( github.ref, 'refs/pull/' )`
-      : `startsWith( github.ref, 'refs/heads/${productionBranch}' )`,
+      : `startsWith( github.ref, 'refs/heads/main' )`,
     concurrency: jobName,
     permissions: {
       contents: JobPermission.READ,
