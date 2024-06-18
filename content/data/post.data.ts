5c20bc89d2b16fd19e0fb5dc4aa03dbc2c63e16b
Cameron Childress <cameronc@sumoc.com>
2024-06-18 16:53:37 -0400

    added vite

--- content/data/post.data.ts
+++ content/data/post.data.ts
@@ -1,11 +1,3 @@
- import { createContentLoader } from "vitepress";
+import { createContentLoader } from "vitepress";
 
-// export default createContentLoader("posts/*.md" /* options */);
-
-export default {
-  load() {
-    return {
-      hello: createContentLoader("posts/*.md" /* options */)
-    }
-  }
-}
+export default createContentLoader("posts/*.md" /* options */);
