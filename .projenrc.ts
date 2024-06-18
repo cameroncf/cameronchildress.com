23ea721dd6f1de726d5c2c79c4bf15c4f92e4e08
Cameron Childress <cameronc@sumoc.com>
2024-06-18 17:09:32 -0400

    wip

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { Project } from "projen";
+import { JsonFile, Project } from "projen";
 
 const project = new Project({ name: "cameronchildress.com" });
 
@@ -8,4 +8,24 @@ project.defaultTask?.exec("npx vite-node .projenrc.ts");
 project.addGitIgnore("content/.vitepress/cache");
 project.addGitIgnore("content/.vitepress/dist");
 
-project.synth()
+// build package.json
+new JsonFile(project, "package.json", {
+  obj: {
+    name: "cameronchildress.com",
+    version: "0.0.0",
+    type: "module",
+    devDependencies: {
+      projen: "^0.82.6",
+      "vite-node": "^1.6.0",
+      vitepress: "^1.2.3",
+      "vitepress-sidebar": "^1.23.2",
+    },
+    scripts: {
+      "docs:dev": "vitepress dev content",
+      "docs:build": "vitepress build content",
+      "docs:preview": "vitepress preview content",
+    },
+  },
+});
+
+project.synth();
