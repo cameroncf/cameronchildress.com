bb26ad872d815b6b3626fbb8135d5b3e1f682781
Cameron Childress <cameronc@sumoc.com>
2024-06-21 20:41:15 -0400

    use an updated version of breeze

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,28 +1,5 @@
 import { NetlifyDeploy, VitepressProject } from "@sumoc/breeze";
 
-/*******************************************************************************
- *
- * VitePress Options
- *
- * Some options we'll use to configure vitepress in this project.
- *
- ******************************************************************************/
-
-/**
- * The subdirectory where the VitePress site itself is located. This value is
- * used to generate lots of other configuration options, including CI workflows.
- */
-// const VITEPRESS_SITE_DIR = "content";
-
-/*******************************************************************************
- *
- * Netlify Options
- *
- * These are the values required for github actions generated at the end of this
- * file.
- *
- ******************************************************************************/
-
 /**
  * The Netlify Site ID is a unique identifier for your site. You can find this
  * value in the Netlify dashboard for your site and replace it with your own
@@ -33,38 +10,16 @@ import { NetlifyDeploy, VitepressProject } from "@sumoc/breeze";
 const NETLIFY_SITE_ID = "7840347a-1605-469f-878f-bc76c7333db4";
 
 /**
- * Notably, we aren't using Netlify to build the site. Instead we're using
- * GitHub Actions for our CI pipeline. The Netlify Auth Token is a secret that
- * you will need to create in your GitHub account and named as shown here.
+ * This component generates a default Vitepress project using Breeze.
  */
-// const NETLIFY_AUTH_TOKEN = "${{ secrets.NETLIFY_AUTH_TOKEN }}";
-
-/**
- * The directory where the VitePress build artifacts are located. This is used
- * to tell various parts of Projen where to expect build outputs to be located.
- */
-// const NETLIFY_DEPLOY_DIR = `${VITEPRESS_SITE_DIR}/.vitepress/dist`;
-
-/*******************************************************************************
- *
- * Projen Config
- *
- * We're using the default TypeScript project, but we're adding some additional
- * configuration to support VitePress. This configures the other app project
- * options. We will also be adding more VitePress specific configurations later
- * in this file.
- *
- ******************************************************************************/
-
 const project = new VitepressProject({
-  /**
-   * This will become the name of the project in the package.json file.
-   */
   name: "cameronchildress.com",
-
-  devDeps: ["@sumoc/breeze", "vitepress-sidebar"],
+  devDeps: ["@sumoc/breeze@^0.0.25", "vitepress-sidebar"],
 });
 
+/**
+ * Add a Netlify deployment task for the site..
+ */
 new NetlifyDeploy(project, { siteId: NETLIFY_SITE_ID });
 
 /**
