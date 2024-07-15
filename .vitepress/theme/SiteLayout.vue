61127852af10f67e1a3b003bd280fe23f4dfcb53
Cameron Childress <cameronc@sumoc.com>
2024-07-15 16:21:32 -0400

    add author & date to posts

--- .vitepress/theme/SiteLayout.vue
+++ .vitepress/theme/SiteLayout.vue
@@ -1,12 +1,24 @@
 <script setup>
+import { useData } from 'vitepress'
 import DefaultTheme from "vitepress/theme";
 const { Layout } = DefaultTheme;
+const { page } = useData();
+
+const foo = (raw) => {
+  const date = new Date(raw);
+  date.setUTCHours(12);
+  return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+}
 </script>
 
 <template>
   <Layout>
-    <!-- <template #aside-outline-before>
-      My custom sidebar top content
-    </template> -->
+    <template #doc-before>
+      <div>Posted By: {{ page.frontmatter.author }} / {{ foo(page.frontmatter.date) }} <br/><br/></div>
+    </template>
   </Layout>
 </template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+// @ts-ignore
 import SiteLayout from "./SiteLayout.vue";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
   Layout: SiteLayout,
-} satisfies Theme;
+} as Theme;
+
